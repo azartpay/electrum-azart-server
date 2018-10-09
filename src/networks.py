25c36c3a059782bb0579a5a6534876858317b8e1
azartpay <dev@azartpay.com>
2018-10-09 10:54:48 +0300

    init

--- src/networks.py
+++ src/networks.py
@@ -1,15 +1,13 @@
-# Main network and testnet3 definitions
-
-# Dash src/chainparams.cpp
+# Azart src/chainparams.cpp
 params = {
-    'dash_main': {
-        'pubkey_address': 76, #L120
-        'script_address': 16, #L122
-        'genesis_hash': '00000ffd590b1485b3caadc19b22e6379c733355108f107a430458cdf3407ab6' #L110
+    'azart_main': {
+        'pubkey_address': 24,
+        'script_address': 25,
+        'genesis_hash': '000009b9903dae4466d48db6c264d711ac554492da34cd0bfa4c0b6d230f29c9'
     },
-    'dash_test': {
-        'pubkey_address': 140, #L220
-        'script_address': 19, #L222
-        'genesis_hash': '00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c' #L210
+    'azart_test': {
+        'pubkey_address': 27,
+        'script_address': 28,
+        'genesis_hash': '0000000000000000000000000000000000000000000000000000000000000000'
     }
 }
